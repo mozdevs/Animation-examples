dbcc65494de0e816fd3e66dbe67f8b241b4eb346
Belén Albeza <belen.albeza@gmail.com>
2016-07-29 12:42:26 +0200

    support better responsiveness (so screen size can change on the fly)

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
 'use strict';
 
+const WIDTH_BREAKPOINT = 900;
+
+function isSmallViewport() {
+    return document.documentElement.clientWidth < WIDTH_BREAKPOINT;
+}
+
 window.onload = function () {
-    var menu = document.querySelector('.sidebar');
-    var burger = document.querySelector('.burger');
-    var viewer = document.querySelector('.viewer-wrapper');
+    let menu = document.querySelector('.sidebar');
+    let burger = document.querySelector('.burger');
+    let viewer = document.querySelector('.viewer-wrapper');
+    let links = Array.from(document.querySelectorAll('a[target=viewer]'));
+
+
+    // -------------------------------------------------------------------------
+    // handle View Source Code button
+    // -------------------------------------------------------------------------
+
+
+    // -------------------------------------------------------------------------
+    // handle responsive menu
+    // -------------------------------------------------------------------------
 
     let hideMenu = function () {
-        viewer.style.display = 'block';
-        menu.style.display = 'none';
-        burger.style.display = 'block';
+        viewer.classList.add('mobile-show');
+        viewer.classList.remove('mobile-hidden');
+
+        menu.classList.add('mobile-hidden');
+        menu.classList.remove('mobile-show');
     };
 
     let showMenu = function () {
-        viewer.style.display = 'none';
-        menu.style.display = 'block';
-        burger.style.display = 'none';
+        viewer.classList.add('mobile-hidden');
+        viewer.classList.remove('mobile-show');
+
+        menu.classList.add('mobile-show');
+        menu.classList.remove('mobile-hidden');
     };
 
-    if (document.documentElement.clientWidth <= 900) {
-        let links = Array.from(document.querySelectorAll('a[target=viewer]'));
-        links.forEach(function(link) {
-            link.addEventListener('click', hideMenu);
-        });
+    links.forEach(function(link) {
+        link.addEventListener('click', hideMenu);
+    });
 
-        burger.querySelector('button').addEventListener('click', showMenu);
-    }
+    burger.querySelector('button').addEventListener('click', showMenu);
 };
